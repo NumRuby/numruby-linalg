36925c3304d91f92c7fb17af0467c04a595a1613
Shane Becker <veganstraightedge@gmail.com>
2025-10-15 03:36:06 -0700

    New gem

---
+++ lib/numruby/linalg.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "linalg/version"
+
+module Numruby
+  module Linalg
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
